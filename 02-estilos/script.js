23610ce737e259ceec8ee501693b882fab8e9b85
MikeyMX332 <163356135+MikeyMX332@users.noreply.github.com>
2024-11-27 12:53:39 -0600

    Asignamos y eliminamos estilos con JS

--- 02-estilos/script.js
+++ 02-estilos/script.js
@@ -47,6 +47,75 @@ A diferencia de CSS, las propiedades de estilo en JS las escribimos usando el fo
 
 CCS-> bakcground-color (kebab-case)
 JS-> backgroundColor (camelCase)
+
 */
 
-console.log("Valores en linea. Color: " +  titulo.style.color + " -Fondo"+titulo.style.backgroundColor )
+console.log("Valores en linea. Color: " +  titulo.style.color + " -Fondo"+titulo.style.backgroundColor );
+
+/* 
+
+Metodo de Propiedad
+
+Asignar el valor de la propiedad de estilo en el elemento seleccionado.
+
+elemento.style.nombrePropiedad = "valor"
+
+-Forma más directa y legible de asignar propiedades.
+-Sólo admite asignar propiedades con formato camelCase
+*/
+
+
+titulo.style.color= "green";
+titulo.style.backgroundColor="red";
+
+console.log("Valores asignados con JS. Color: " +  titulo.style.color + " -Fondo"+titulo.style.backgroundColor );
+
+/* 
+
+Método setPropety ()
+
+Asignamos una propiedad de estilo al elemento seleccionado.
+
+elemento.style.setPropety(nombrePropiedad, valor, impotant);
+
+-Más flexible y poderoso.
+-Las Propiedades se escriben en formato kebab-case
+-Parametro "Important" es opcional.
+
+*/
+
+
+titulo.style.setProperty("color", "darkblue");
+titulo.style.setProperty("background-color", "orange", "important");
+
+console.log("Valores asignados con setProperty. Color: " +  titulo.style.color + " -Fondo"+titulo.style.backgroundColor );
+
+/* 
+
+Elminar valores de propiedades.
+
+Usamos el método de propiedad y le definimos una cadena de texto vacia.
+
+elemnto.style.nomrePropiedad=""
+
+Esto elimina los valores previamente definidos en JS y en los estilos en linea
+
+*/
+
+titulo.style.color="";
+titulo.style.backgroundColor=""; 
+
+/* 
+
+Metodo removeProperty ()
+
+Elimina la propiedad de estilo.
+
+  elemento.style.removeProperty(nombrePropiedad)
+
+  -Las propiedades se pasan en formato kebab-case
+
+*/
+
+titulo.style.removeProperty("color");
+titulo.style.removeProperty("background-color")
