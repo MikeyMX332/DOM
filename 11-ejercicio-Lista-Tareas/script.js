e7b07f1cc5de6446348c97d3c912715fda777760
MikeyMX332 <163356135+MikeyMX332@users.noreply.github.com>
2025-01-29 17:07:42 -0600

    Iniciamos funcionalodad de la lista de tareas

---
+++ 11-ejercicio-Lista-Tareas/script.js
@@ -0,0 +1,12 @@
+/* Referencia */
+const taskInput =document.getElementById("taskInput")
+const addButton=document.getElementById ("addButton")
+
+/* Escihador */
+addButton.addEventListener('click'()=>{
+    console.log(taskInput)
+})
+
+function createTask () {
+    if(taskInput,value=true)
+}
